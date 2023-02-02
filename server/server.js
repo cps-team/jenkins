3f52eacc9aec92eb324acb48b7e4b43791df5708
Johngreen <ramses203@naver.com>
2023-02-02 18:01:40 +0900

    test

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 4000;
+var app = express();
+const User = require('./route/User');
+const Factory = require('./route/Factory');
+const Machine = require('./route/Machine');
+const animation_table = require('./route/animation_table');
+
+// 크로스 오리진 이슈
+app.use(cors());
+app.use(express.json())
+app.use('/User', User);
+app.use('/Factory', Factory);
+app.use('/Machine', Machine);
+app.use('/animation_table', animation_table);
+
+// 리액트 빌드파일을 서버에 제공
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server On : http://localhost:${PORT}/`);
+});
+
+
+
+
+
+
+
