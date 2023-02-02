dc163d8e07f12c0bb5afab165b2b1128d4417011
Johngreen <ramses203@naver.com>
2023-02-02 17:08:35 +0900

    testtest

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ app.use('/animation_table', animation_table);
 
 // 리액트 빌드파일을 서버에 제공
 // app.use(express.static("public"));
-// app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
