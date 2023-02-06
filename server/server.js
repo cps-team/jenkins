548d4cb4f2f07c959776c92c07827a03e4bdf664
Johngreen <ramses203@naver.com>
2023-02-06 13:28:40 +0900

    test

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use('/animation_table', animation_table);
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 
