34ec03d0880360ab152693ad22689ca5eef98f6c
Johngreen <ramses203@naver.com>
2023-02-07 17:01:11 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,16 +3,14 @@ const cors = require('cors');
 const path = require('path');
 
 const PORT = process.env.PORT || 4000;
-// console.log('process.env : ', process.env)
 var app = express();
 const User = require('./route/User');
 const Factory = require('./route/Factory');
 const Machine = require('./route/Machine');
 const animation_table = require('./route/animation_table');
 
-// 크로스 오리진 이슈
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use('/User', User);
 app.use('/Factory', Factory);
 app.use('/Machine', Machine);
@@ -20,11 +18,9 @@ app.use('/animation_table', animation_table);
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
+
+    setTimeout(() => {
+        console.log(`Server shutting down after 3 minutes`);
+        process.exit();
+    }, 1 * 10 * 1000);
 });
-
-
-
-
-
-
-
