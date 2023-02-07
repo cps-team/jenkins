b14c25135bc10ca4a508510162a499ad9c9c57d0
Johngreen <ramses203@naver.com>
2023-02-07 14:08:55 +0900

    Delete killNode.sh

--- killNode.sh
+++
@@ -1,7 +0,0 @@
-PROC=`ps aux | grep web-test`
-if [[ $PROC == *"node"* ]]; then
-    echo "Process is running."
-    kill -15 `ps -ef | grep node | grep -v grep | awk '{print $2}'`
-else
-    echo "Process is not running."
-fi
