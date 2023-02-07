435f63e28b62c2c128888895c50a896cb5951103
Johngreen <ramses203@naver.com>
2023-02-07 14:41:15 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ app.use('/Factory', Factory);
 app.use('/Machine', Machine);
 app.use('/animation_table', animation_table);
 
+console.log('test,sets');
+
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
 });
